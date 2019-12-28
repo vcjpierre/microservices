44235a493ff3176d52e98841a75af29db98b6522
Jean Pierre <vcjpierre@gmail.com>
2019-12-28 00:19:23 -0400

    Created resolvers and services

--- api-gateway/src/graphql/resolvers/Query/listings.js
+++ api-gateway/src/graphql/resolvers/Query/listings.js
@@ -1,11 +1,7 @@
+import ListingsService from "#root/adapters/ListingsService";
+
 const listingsResolver = async () => {
-    return [
-        {
-            id: 1,
-            title: "test",
-            description: "booyah"
-        }
-    ];
+    return await ListingsService.fetchAllListings();
 };
 
 export default listingsResolver;
--- users-service/src/server/startServer.js
+++ users-service/src/server/startServer.js
@@ -4,6 +4,8 @@ import express from "express";
 
 import accessEnv from "#root/helpers/accessEnv";
 
+import setupRoutes from "./routes";
+
 const PORT = accessEnv("PORT", 7101);
 
 const app = express();
@@ -13,16 +15,11 @@ app.use(bodyParser());
 app.use(
     cors({
         origin: (origin, cb) => cb(null, true),
-        credentials: true,
-        preflightContinue: true,
-        exposedHeaders: [
-            "Access-Control-Allow-Headers",
-            "Access-Control-Allow-Origin, Origin, X-Requested-With, Content-Type, Accept",
-            "X-Password-Expired"
-        ],
-        optionsSuccessStatus: 200
+        credentials: true        
     })
 );
+
+setupRoutes(app);
 
 app.use((err, req, res, next) => {
     return res.status(500).json({
