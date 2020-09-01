a0835af8bc69716100c8475438a458f13cc8fd24
Jean Pierre Velez <vcjpierre@gmail.com>
2020-08-31 23:38:44 -0400

    Check cache should be before accessing the process.env

--- api-gateway/src/helpers/accessEnv.js
+++ api-gateway/src/helpers/accessEnv.js
@@ -1,16 +1,18 @@
 const cache = {};
 
 const accessEnv = (key, defaultValue) => {
+    if (cache[key]) return cache[key];
+
     if (!(key in process.env)) {
         if (defaultValue) return defaultValue;
-        throw new Error(`${key} not found in process.env!`);
-    }
-
-    if (cache[key]) return cache[key];
+        throw new Error(
+          `${key} not found in process.env! and default value was not set`
+        );
+    }    
 
     cache[key] = process.env[key];
 
-    return process.env[key];
+    return cache[key];
 };
 
 export default accessEnv;
